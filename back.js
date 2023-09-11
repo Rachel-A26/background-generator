281cb653be4627c97e693a4a079f7ebadf217da6
Rachel-A26 <84336637+Rachel-A26@users.noreply.github.com>
2023-09-11 13:56:50 +0530

    addding starting project

---
+++ back.js
@@ -0,0 +1,38 @@
+// var c=document.querySelector("h3");
+// var color1=document.querySelector(".color1");
+// var color2=document.querySelector(".color2");
+// var body=document.getElementById("gradient");
+// var btn=document.getElementByClassName("hi");
+// console.log(btn);
+// function generate()
+// {
+//     Math.random();
+// }
+// function setvalue()
+// {
+//     body.style.background="linear-gradient(to right,"+color1.value+","+color2.value+")";
+//     CSS.textContent=body.style.background + ";";
+// }
+
+
+// color1.addEventListener("input",setvalue);
+
+
+
+// color2.addEventListener("input",setvalue);
+// function generate()
+// {
+//     document.write(Math.random());
+// }
+
+// btn.addEventListener("click",generate);
+
+var a = 0;
+function q2() {
+    a = 5;
+}
+
+function q22() {
+    alert(a);
+}
+
